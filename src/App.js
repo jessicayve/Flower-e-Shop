af869983cda52f240f785dbf2106d138973f14ae
jessicayve <yve.jessica@gmail.com>
2023-01-31 19:39:14 -0300

    font changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import Home from './pages/Home'
+import Home from './pages/Home';
 import { createGlobalStyle } from "styled-components";
+
+
+
 export const GlobalStyled = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Source Sans Pro', sans-serif;
   }
 `;
 
